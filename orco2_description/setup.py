a0c7249ff56180e13f4b0b9b1b4cd81dbbe296c3
daviddorf2023 <daviddorf2023@u.northwestern.edu>
2024-02-20 13:35:48 -0500

    Initial commit with source code to launch orco2 in RViz, MoveIt, and Gazebo.

---
+++ orco2_description/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'orco2_description'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+        (os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
+        (os.path.join('share', package_name, 'config'), glob('config/*'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='author',
+    maintainer_email='davidjohndorf@gmail.com',
+    description='The ' + package_name + ' package',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'joint_pub_example = orco2_description.joint_pub_example:main',
+        ],
+    },
+)
